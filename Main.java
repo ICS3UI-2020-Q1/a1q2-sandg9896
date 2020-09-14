e3074ed2b3d577d0fd494ef5bd0ce38ba116cbd6
sandg9896 <sandg9896@wrdsb.ca>
2020-09-14 15:25:14 +0000

    modulus

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Gavin
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // declare a variable that will calcualte the quotient of two numbers and the remainder
+    String name;
+   System.out.println("Enter two numbers using X and Y to calcualte the quotient and the remainder:");
     
+    // Creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+     int X = input.nextInt();
+
+     // Creates a Scanner used for input
+     int Y = input.nextInt();
+
+
+     int Z = (X/Y);
+     int P = (X%Y);
+     
+
+     // declare and calculate the quotient
+   System.out.println(X + "divided by" + Y + "is equal to" + Z + "with a remainder of" + P);
+   
+
+
   }
 }
